3887921f268718e2a0e59232ce07034ec69619e9
Sean Doyle <sdoylelambda@gmail.com>
2019-03-25 18:05:59 -0400

    Components basic layout with borders and constraints.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -4,7 +4,7 @@ import './Header.css';
 const ImageThumbnail = () => {
     return (
     <div className="lambdaLogo">
-        <img src="https://ibin.co/3whrpKSBbZ81" alt="Lambda"/>
+        <img src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" alt="Lambda"/>
     </div>
     )
 }
