5b0751b5ccf38918fcf78fe5e306e25e0abd699b
Sean Doyle <sdoylelambda@gmail.com>
2019-03-25 18:37:14 -0400

    Reactjs.org link component at bottom of page.

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './App.css';
+import './App.scss';
 
 import Header from './components/HeaderComponents/HeaderContainer';
 import HeaderContent from './components/HeaderComponents/HeaderContent';
 import CardContent from './components/CardComponents/CardContent'
 import ImageThumbnail from './components/HeaderComponents/ImageThumbnail';
 import CardImageThumbnail from './components/CardComponents/CardImageThumbnail';
+import CardBanner from './components/CardComponents/CardBanner';
 
 const App = () => {
   return (
     <div className="App">
-    <ImageThumbnail />
-    <Header />
-    <HeaderContent />
-    <CardImageThumbnail />
-    <CardContent />
-
-      
+      <div className="testing">
+        <ImageThumbnail />
+        <Header />
+        <HeaderContent />
+        <CardImageThumbnail />
+        <CardContent />
+        <CardBanner />
+      </div>
     </div>
   );
 };
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => {
+    return (
+        <div>
+            <a href="www.reactjs.org">reactjs.org</a>
+        </div>
+    )
+}
+
+export default CardBanner;
