1f53f0998dcbd865f8e358793666c4792885fc7d
Sean Doyle <sdoylelambda@gmail.com>
2019-03-25 18:48:54 -0400

    6:45pm

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -3,7 +3,7 @@ import './Card.css';
 
 const CardBanner = () => {
     return (
-        <div>
+        <div className="reactJS">
             <a href="www.reactjs.org">reactjs.org</a>
         </div>
     )
