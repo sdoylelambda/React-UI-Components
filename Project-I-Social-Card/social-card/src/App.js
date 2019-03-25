d60e090553b72af16a0a08dcabe0fa87e7a05c1d
Sean Doyle <sdoylelambda@gmail.com>
2019-03-25 16:03:24 -0400

    Starting page working.

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-import Header from './components/HeaderComponents';
+import Header from './components/HeaderComponents/HeaderContainer';
 
 const App = () => {
   return (
