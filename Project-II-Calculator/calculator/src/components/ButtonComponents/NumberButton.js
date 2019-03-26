bf920a715ff866d83f469b27a9e234cdd24f282a
Sean Doyle <sdoylelambda@gmail.com>
2019-03-26 17:22:37 -0400

    Numbers in correct order.

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Button.css';
 
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
 
 const NumberButton = () => {
     return (
@@ -13,4 +13,3 @@ const NumberButton = () => {
 
 export default NumberButton;
 
-// displays 0 1 2 3 4 5 6 7 8 9 
