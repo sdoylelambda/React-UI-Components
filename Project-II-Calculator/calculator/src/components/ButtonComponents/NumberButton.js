0c12c25fbbf4c7412169d8f9e654cc481baa2271
Sean Doyle <sdoylelambda@gmail.com>
2019-03-26 16:25:13 -0400

    Numbers .map showing, no style.

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Button.css';
 
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 const NumberButton = () => {
     return (
-    <div className="numberButton">"NumberButton"</div>
+    // <div className="numberButton">"NumberButton"</div>
+    <div>
+        {numbers.map(x => x) };
+            
+    
+
+    </div>
     )
 }
 
 export default NumberButton;
+
+// displays 0 1 2 3 4 5 6 7 8 9 
